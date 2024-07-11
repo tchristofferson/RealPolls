bddc2dec49f0fecbf41119f0fa0b4db5ede51788
tchristofferson <longleadtrc@gmail.com>
2024-07-10 20:27:21 -0500

    Initial commit

---
+++ backend/src/main/java/com/tchristofferson/realpolls/RealPollsApplication.java
@@ -0,0 +1,13 @@
+package com.tchristofferson.realpolls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealPollsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealPollsApplication.class, args);
+    }
+
+}
